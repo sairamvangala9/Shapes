a11f70dbebbf8262e57a9471dcf8a3c6879cd601
jmohankumar <mohan.jagabatthula@ggktech.com>
2019-01-03 10:04:05 +0530

    main class and circle class

---
+++ Circle.cs
@@ -0,0 +1,8 @@
+public class Circle
+{
+	public int radius;
+	public int AreaCircle()
+	{
+		return (int)(Math.PI*radius*radius);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Circle circle=new Circle();
+		circle.radius=7;
+		Console.Write(circle.AreaCircle());
+	}
+}
