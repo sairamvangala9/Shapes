1411deafe8f5f81efdf91d11deb1652f7f3555bd
Shiva Prasad <svsshivaprasad@gmail.com>
2019-01-03 10:45:48 +0530

    Update Circle.cs

--- Circle.cs
+++ Circle.cs
@@ -1,8 +1,20 @@
 public class Circle
 {
-	public int radius;
-	public double AreaCircle()
+	private double radius;
+	public Circle(double radius)
+	{
+		this.radius=radius;
+	}
+	public Circle()
+	{
+		this.radius=0;
+	}
+	public double AreaOfCircle()
 	{
 		return Math.PI*radius*radius;
+	}
+	public double PerimeterOfCircle()
+	{
+		return 2*Math.PI*radius;
 	}
 }
