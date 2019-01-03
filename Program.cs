0c44b68264cc0a5ba0c5d7c2487cd0163aa271d7
Shiva Prasad <svsshivaprasad@gmail.com>
2019-01-03 10:46:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ public class Program
 {
 	public static void Main()
 	{
-		Circle circle=new Circle();
-		circle.radius=7;
-		Console.Write(circle.AreaCircle());
+		Circle circle=new Circle(7.88);
+		Console.Write(circle.AreaOfCircle());
 	}
 }
